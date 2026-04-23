2e5a2d25728dc0ab74f9194e59fdab98dac9bc15
Anderson Alves <anderson.filho.xap@gmail.com>
2026-04-22 21:38:17 -0300

    Modifiquei a estrutura!

--- Sistema de Login/login.py
+++ Sistema de Login/login.py
@@ -1,28 +1,60 @@
 from time import sleep
-print('='*50)
-login = str(input('Crie um login para acessar o programa: '))
-senha = str(input('Crie uma senha para acessar o progama: '))
+def senha_forte(senha_aleatoria):
+        procurando_caracter = len(senha_aleatoria.strip())
+        if procurando_caracter <= 8:
+            print('\033[31mSenha Fraca!\033[m Digite mais de 8 caracteres!')
+            while True:
+                tentar_novamente = str(input('Senha errada tente novamente: \033[32m'))
+                print(end='\033[m')
+                procurando_caracter_novamente = len(tentar_novamente.strip())
+                if procurando_caracter_novamente <= 8:
+                     continue
+                else:
+                     break
+        else:
+            print('\033[34mSenha Forte!\033[m')
+            return True
+def email(email_nao_encontrado):
+    if not '@gmail.com' in email_nao_encontrado:
+        print('\033[31memail nao encontrado\033[m')
+        while True:
+                tentar_novamente =  str(input('Login errado tente novamente: \033[32m'))
+                print(end='\033[m') 
+                if not '@gmail.com' in tentar_novamente:
+                     continue
+                else:
+                     break
+    else:
+        print('\033[34mEmail encontrado\033[m')
+        return True
 print('='*50)
 print('='*15,'Bem vindo ao sistema simples de login','='*15)
-tentativa = str(input('login: '))
-senha2 = str(input('Digite a Senha: '))
+print('='*50)
+print('Crie um login para acessar o programa')
+tentativa = email(str(input('login: \033[32m')))
+print(end='\033[m')
+print('='*50)
+print('Crie uma senha para acessar o progama')
+senha2 = senha_forte(str(input('Digite a Senha:  \033[32m')))
+print(end='\033[m')
 print('='*50)
 print('Conferindo...')
 sleep(1)
+print('\033[32mEmail e Senha Fortes!\033[m')
 print('='*50)
-while tentativa != login or senha2 != senha:
-    tentativa2 = str(input('Login errado tente novamente: '))
-    senha3 = str(input('Senha errada tente novamente: '))
-    print('Conferindo...')
-    sleep(1)
-    if tentativa2 == login:
-        if senha3 == senha:
-            break
-        else:
-            print(' '*30,'\033[31mSenha incorreta!\033[m')
-            print('='*50)
-            continue
-    else:
-        print(' '*30,'\033[31mLogin Errado!\033[m')
-        print('='*50)
-        continue
+#while tentativa != True or senha2 != senha:
+#    tentativa2 = str(input('Login errado tente novamente: '))
+#    senha3 = str(input('Senha errada tente novamente: '))
+#    print('Conferindo...')
+#    sleep(1)
+#    if tentativa2 == login:
+#        if senha3 == senha:
+#            break
+#        else:
+#            print(' '*30,'\033[31mSenha incorreta!\033[m')
+#            print('='*50)
+#            continue
+#    else:
+#        print(' '*30,'\033[31mLogin Errado!\033[m')
+#        print('='*50)
+#        continue
