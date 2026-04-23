dc467c8616bb86fc7af46f252382fd62721804bf
Anderson Alves <anderson.filho.xap@gmail.com>
2026-04-23 17:35:27 -0300

    Pronto Ajustado!

--- Sistema de Login/login.py
+++ Sistema de Login/login.py
@@ -4,7 +4,7 @@ def senha_forte(senha_aleatoria):
         if procurando_caracter <= 8:
             print('\033[31mSenha Fraca!\033[m Digite mais de 8 caracteres!')
             while True:
-                tentar_novamente = str(input('Senha errada tente novamente: \033[32m'))
+                tentar_novamente = str(input('Senha errada tente novamente: \033[35m'))
                 print(end='\033[m')
                 procurando_caracter_novamente = len(tentar_novamente.strip())
                 if procurando_caracter_novamente <= 8:
@@ -13,13 +13,13 @@ def senha_forte(senha_aleatoria):
                      senha_aleatoria = procurando_caracter_novamente
                      break
         else:
-            print('\033[34mSenha Forte!\033[m')
+            print('\033[32mSenha Forte!\033[m')
             return senha_aleatoria
 def email(email_nao_encontrado):
     if not '@gmail.com' in email_nao_encontrado:
         print('\033[31memail nao encontrado\033[m')
         while True:
-                tentar_novamente =  str(input('Login errado tente novamente: \033[32m'))
+                tentar_novamente =  str(input('Login errado tente novamente: \033[35m'))
                 print(end='\033[m') 
                 if not '@gmail.com' in tentar_novamente:
                      continue
@@ -27,7 +27,7 @@ def email(email_nao_encontrado):
                      email_nao_encontrado = tentar_novamente
                      break
     else:
-        print('\033[36mEmail encontrado\033[m')
+        print('\033[32mEmail encontrado\033[m')
         return email_nao_encontrado
 print('='*50)
 print('='*10,'Bem vindo ao sistema simples de login','=')
@@ -48,5 +48,13 @@ entrar = str(input(('Digite o login para acessar sua pagina: \033[32m')))
 print(end='\033[m')
 senha_entrar = str(input('Digite sua senha para acessar: \033[32m'))
 print(end='\033[m')
-print(tentativa,senha)
-#print('\033[31mUsuario ou Senha Errados!\033[m')
+while entrar != tentativa or senha_entrar != senha:
+     if entrar != tentativa or senha_entrar != senha:
+          print('\033[31mUsuario ou Senha Errados!\033[m Tente Novamente em Aqui em Baixo!')
+          print('='*50)
+          entrar = str(input(('Digite o login para acessar sua pagina: \033[32m')))
+          print(end='\033[m')
+          senha_entrar = str(input('Digite sua senha para acessar: \033[32m'))
+          print(end='\033[m')
+     elif entrar == tentativa and senha_entrar == senha:
+          break
