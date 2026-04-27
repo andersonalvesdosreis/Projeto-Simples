99477be8bdd41e0e4c109919fec083577dc016cb
Anderson Alves <anderson.filho.xap@gmail.com>
2026-04-27 12:30:10 -0300

    Ajustei aq

--- Machine Learning/sistema.py
+++ Machine Learning/sistema.py
@@ -1,3 +1,29 @@
+import json
+import nltk
+from nltk.stem import PorterStemmer
+nltk.download('punkt')
+nltk.download('punkt_tab')
+stemmer = PorterStemmer()
 import torch
-def dados_de_treinamentos(entrada,saida):
-    
+import torchtext
+from torchtext.data.utils import get_tokenizer
+import os
+data = {
+    "intents": [
+        {"tag": "ajuda", "patterns": ["Preciso de ajuda", "ajude-me"], "responses": ["Estou aqui!"]}
+    ]
+}
+
+words = []
+classes = []
+documents = []
+
+for intent in data['intents']:
+    for pattern in intent['patterns']:
+        w = nltk.word_tokenize(pattern)
+        words.extend(w)
+        documents.append((w, intent['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+words = [stemmer.stem(w.lower()) for w in words if w != '?']
+words = sorted(list(set(words)))
